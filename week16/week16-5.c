dd196e7dd10bf222e3291fc89b42f13bebd8f10b
賴勁舟 <91170808+Zhouuu11111@users.noreply.github.com>
2021-12-29 11:37:14 +0800

    Add files via upload

---
+++ week16/week16-5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+char line[300]="10101001010101010101011010001001010100";
+int main()
+{
+
+    int ans=0;
+
+    for(int i=0; line[i] != '\0';i++){
+        ///int i=0;
+       /// while ( line[i] == '1') ans++;
+       if( line[i] == '1') ans++;
+
+
+    }
+    printf("¦³%d­Ó1\n", ans);
+}
